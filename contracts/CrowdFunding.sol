f646d4dd520c529712a556f78b9fef3a6e6506f0
sahar <sahar.choudhury@city.ac.uk>
2025-03-05 01:21:47 +0000

    added components and complete the first smartcontract , yet to be tested

---
+++ contracts/CrowdFunding.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract CrowdFunding {
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    // there is no need to create a constrcutor for this contract if youre wondering why its missing
+
+    mapping(uint256 => Campaign) public campaigns;
+    uint256 public numberOfCampaigns = 0;
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline
+    ) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+        require(
+            campaign.deadline < block.timestamp,
+            "the deadline should be set the future as this date has passed. "
+        );
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+        // to return the actual length of the campaigns
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+        Campaign storage campaign = campaigns[_id];
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+        // this line allows us to send fund to the owner of the contract or campaign rather than the contract account itself
+
+        if (sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+    function getDonators(
+        uint256 _id
+    ) public view returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+
+    function getCampaign() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for (uint i = 0; 1 < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+            allCampaigns[i] = item;
+        }
+        return allCampaigns;
+    }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
-
+// when ever i make componenets is best to throw it in my app.js 
+// internal import 
+import { NavBar,Footer } from "../Components";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <NavBar/>
+    <Component {...pageProps} />
+    <Footer/>
+    </>
+
+  )
 }
